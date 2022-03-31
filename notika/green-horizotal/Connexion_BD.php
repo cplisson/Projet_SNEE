bea2308b3b041f80e2134cf239d67143ca5fe5e0
Ben <benjaminherman.pro@gmail.com>
2022-03-31 14:42:31 +0200

    correction

--- notika/green-horizotal/Connexion_BD.php
+++ notika/green-horizotal/Connexion_BD.php
@@ -1,7 +1,7 @@
 <?php
             $servername = 'localhost';
             $username = 'root';
-            $password = 'root';
+            $password = '';
             $dbname = 'snee';
             //On essaie de se connecter
 			

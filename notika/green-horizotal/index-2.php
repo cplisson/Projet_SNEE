b43d417a1c6c2e8e9b6da544187a1718ef8d4894
ChloePlisson <chloeplisson@gmail.com>
2022-04-07 14:54:01 +0200

    mofif site

--- notika/green-horizotal/index-2.php
+++ notika/green-horizotal/index-2.php
@@ -21,7 +21,7 @@ $Description = $row[0]["description"];
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Dashboard One | Notika - Notika Admin Template</title>
+    <title>Visualisation des transmissions | Notika - Notika Admin Template</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
@@ -147,9 +147,9 @@ $Description = $row[0]["description"];
                     <div class="mobile-menu">
                         <nav id="dropdown">
                             <ul class="mobile-menu-nav">
-                                <li><a data-toggle="collapse" data-target="#Charts" href="#">Home</a>
+                                <li><a data-toggle="collapse" data-target="#Charts" href="#">Acceuil</a>
                                     <ul class="collapse dropdown-header-top">
-                                        <li><a href="index.html">Dashboard One</a></li>
+                                        <li><a href="index.html">Visualisation des transmissions</a></li>
                                         <li><a href="index-2.php">Dashboard Two</a></li>
                                         <li><a href="index-3.html">Dashboard Three</a></li>
                                         <li><a href="index-4.html">Dashboard Four</a></li>
@@ -249,7 +249,7 @@ $Description = $row[0]["description"];
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <ul class="nav nav-tabs notika-menu-wrap menu-it-icon-pro">
-                        <li class="active"><a data-toggle="tab" href="#Home"><i class="notika-icon notika-house"></i> Home</a>
+                        <li class="active"><a data-toggle="tab" href="#Acceuil"><i class="notika-icon notika-house"></i> Acceuil</a>
                         </li>
                         <li><a data-toggle="tab" href="#mailbox"><i class="notika-icon notika-mail"></i> Email</a>
                         </li>
@@ -267,9 +267,9 @@ $Description = $row[0]["description"];
                         </li>
                     </ul>
                     <div class="tab-content custom-menu-content">
-                        <div id="Home" class="tab-pane in active notika-tab-menu-bg animated flipInX">
+                        <div id="Acceuil" class="tab-pane in active notika-tab-menu-bg animated flipInX">
                             <ul class="notika-main-menu-dropdown">
-                                <li><a href="index.html">Dashboard One</a>
+                                <li><a href="index.html">Visualisation des transmissions</a>
                                 </li>
                                 <li><a href="index-2.php">Dashboard Two</a>
                                 </li>
